32f2f1e2d6a7dc02849c776eebc48c98a3b1d4a8
Andy Huang <130339280+anh1231@users.noreply.github.com>
2026-05-21 13:52:19 -0700

    Add: design-twitter-feed - submission-8

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-8.py
@@ -0,0 +1,33 @@
+class Twitter:
+
+    def __init__(self):
+        self.tweets = defaultdict(list)
+        self.followers = defaultdict(dict)
+        self.feed = deque()
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweets[userId].append(tweetId)
+        self.feed.append([userId, tweetId])
+        if userId not in self.followers:
+            self.followers[userId][userId] = 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        current_feed = self.feed.copy()
+        res = []
+        if not current_feed:
+            return res
+        count = 10
+        while count and current_feed:
+            user, tweet = current_feed.pop()
+            if user in self.followers[userId]:
+                if self.followers[userId][user] == 1:
+                    res.append(tweet)
+                    count -= 1
+        return res
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.followers[followerId][followeeId] = 1
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followerId != followeeId and followeeId in self.followers[followerId]:
+            self.followers[followerId].pop(followeeId)
